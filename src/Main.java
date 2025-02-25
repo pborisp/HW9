ab1aeb42f132bba5578353ed8caa04ba1e20c3d7
pborisp <popovbs@gmail.com>
2025-02-25 23:37:27 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        // Задача №1
+        System.out.println("Задача №1:");
+        String firstName = new String("Ivan");
+        String middleName = new String("Ivanovich");
+        String lastname = new String("Ivanov");
+        String fullName = new String();
+        fullName = lastname + ' ' + firstName + ' ' + middleName;
+        System.out.println(fullName);
+        System.out.println();
     }
 }
